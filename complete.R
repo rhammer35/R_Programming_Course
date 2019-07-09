6e1fa50d59f04fd9c046480468ae608bd486eb4e
rhammer35 <42186538+rhammer35@users.noreply.github.com>
2019-07-09 15:07:17 -0400

    Add files via upload

--- complete.R
+++ complete.R
@@ -3,12 +3,17 @@ complete <- function(directory, id = 1:332) {
     i <- id[1]
     k <- 1
     id1 <- dir(directory)
+    nobs <- c()
     
-    while(i < j) {
-        i <- i + 1
-        name2 <- paste(directory, "/", id1[i], sep = "")
-        table <- rbind(table, as.matrix(read.csv(name2)))
+    while(k <= j) {
+        name1 <- paste(directory, "/", id1[i], sep = "")
+        current_file <- read.csv(name1)
+        count1 <- sum(as.numeric(complete.cases(current_file)))
+        nobs <- c(nobs, count1)
+        k <- k + 1
+        i <- id[k]
     }
-    my_mean <- mean(as.numeric(table[,pollutant]),na.rm = TRUE)
-    my_mean
+    
+    my_result <- data.frame(id, nobs)
+    print(my_result)
 }
