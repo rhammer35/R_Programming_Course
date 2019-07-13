91d2fecb9097af7607cab3fde6ad18347ec6ddda
rhammer35 <rhammer35@icloud.com>
2019-07-13 12:50:21 -0400

    Added code to check for validity of input values with source data.

--- best.R
+++ best.R
@@ -1,7 +1,19 @@
 best <- function(state, outcome) {
+    ## initialize character vector of possible outcomes for check later
+    poss_outcomes <- c("heart attack", "heart failure", "pneumonia")
+    
     ## Read outcome data
+    source_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
     
     ## Check that state and outcome are valid
+    if(as.character(state) != source_data[, 7]) {
+        stop("invalid state")
+    }
+    
+    else if (as.character(outcome) != poss_outcomes) {
+        stop("invalid outcome")
+    }
+    
     
     ## Return hospital name in that state with lowest 30-day death
     ## rate
